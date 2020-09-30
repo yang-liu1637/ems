124250e24bd0599817465b275618d67f2ae4a472
yang-liu1637 <liuyang_1637@163.com>
2020-09-30 18:01:22 +0800

    更新main

--- new/project/src/app/main/main.component.ts
+++ new/project/src/app/main/main.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {DoUsersService} from '../service/doUsers.service';
 import {MainUsersService} from '../service/mainUsers.service';
+import {Subject} from 'rxjs';
+import {debounceTime, distinctUntilChanged} from 'rxjs/operators';
 
 
 
@@ -13,7 +15,9 @@ import {MainUsersService} from '../service/mainUsers.service';
 })
 export class MainComponent implements OnInit {
   collection: any = [];
-
+  public subject = new Subject();
+  private searchTerms = new Subject<string>();
+  res = false;
   constructor(private users: MainUsersService) { }
 
   ngOnInit(): void {
@@ -21,6 +25,28 @@ export class MainComponent implements OnInit {
       // 订阅server里面的list
       this.collection = result;
     });
+    this.subject.pipe(debounceTime(500)).subscribe(() => { // 请求防抖 500毫秒
+        console.log(11); // 防抖成功 控制台返回11
+      }
+    );
+    this.searchTerms
+      .pipe(
+        // 请求防抖 300毫秒
+        debounceTime(300),
+        distinctUntilChanged())
+      .subscribe(() => {// 后续操作
+        this.res = true;
+        console.log(this.res); // 用户输入有效控制台刷新res为true的次数
+      });
+  }
+
+  search(searchParam: string): void {
+    this.searchTerms.next(searchParam);
+  }
+
+  public demo(): void { // 单击demo 控制台返回22 若单击有效 则返回subject11
+    console.log(22);
+    this.subject.next();
   }
 }
 
