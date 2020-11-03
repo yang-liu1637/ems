22ecf1e10880d468fc90d8ccc606c527a957fc99
yang-liu1637 <liuyang_1637@163.com>
2020-11-03 18:02:02 +0800

    11.3

--- Store/src/app/app.component.spec.ts
+++ Store/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'antProject'`, () => {
+  it(`should have as title 'Store'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('antProject');
+    expect(app.title).toEqual('Store');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('antProject app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Store app is running!');
   });
 });
